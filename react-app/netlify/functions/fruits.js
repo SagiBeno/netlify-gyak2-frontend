9f67c822e86cdfd1bf8c35a829ff579b45d2d51a
SagiBeno <benosagi321@gmail.com>
2026-01-22 09:54:14 +0100

    Add parameters

--- react-app/netlify/functions/fruits.js
+++ react-app/netlify/functions/fruits.js
@@ -1,15 +1,17 @@
 /** endpoint: /.netlify/functions/fruits */
 
-export const handler = async () => {
+export const handler = async (event, context) => {
 
-    console.log('endpoint /fruits')
+    console.log('endpoint /fruits called');
+    console.log('event:', event);
+    console.log('context:', context);
 
     return {
         statusCode: 200,
         body: JSON.stringify([
             {id: 1, name: 'orange', healthy: true},
             {id: 2, name: 'banana', healthy: true},
-            {id: 2, name: 'dates', healthy: false},
+            {id: 3, name: 'dates', healthy: false},
         ])
     }
 }
