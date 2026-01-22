cf524e1ec2bc6ce0baad3ffd52406ad37d9af53c
SagiBeno <benosagi321@gmail.com>
2026-01-22 09:46:55 +0100

    Implement GET /fruits endpoint

---
+++ react-app/netlify/functions/fruits.js
@@ -0,0 +1,15 @@
+/** endpoint: /.netlify/functions/fruits */
+
+export const handler = async () => {
+
+    console.log('endpoint /fruits')
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify([
+            {id: 1, name: 'orange', healthy: true},
+            {id: 2, name: 'banana', healthy: true},
+            {id: 2, name: 'dates', healthy: false},
+        ])
+    }
+}
