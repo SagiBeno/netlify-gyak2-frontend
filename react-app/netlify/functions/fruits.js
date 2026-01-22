ac9cf674f69728ba81f71e08ba876989c2bc1b9a
SagiBeno <benosagi321@gmail.com>
2026-01-22 11:24:54 +0100

    Log POST result

--- react-app/netlify/functions/fruits.js
+++ react-app/netlify/functions/fruits.js
@@ -11,17 +11,13 @@ export const handler = async (event, context) => {
 
     const sql = neon(); // automatically uses env NETLIFY_DATABASE_URL
 
-    const fruits = await sql`SELECT * FROM fruits LIMIT 50`;
+    const fruits = await sql`SELECT * FROM fruits ORDER BY id LIMIT 50`;
     console.log('fruits', fruits)
 
     if (event.httpMethod == 'GET') {
         return {
             statusCode: 200,
-            body: JSON.stringify([
-                {id: 1, name: 'orange', healthy: true},
-                {id: 2, name: 'banana', healthy: true},
-                {id: 3, name: 'dates', healthy: false},
-            ])
+            body: JSON.stringify(fruits)
         }
     }
 
@@ -29,8 +25,12 @@ export const handler = async (event, context) => {
         console.log('body:', event.body); // JSON string
         const newFruit = JSON.parse(event.body);
         console.log('newFruit', newFruit); // Object {}
+
+        const result = await sql`INSERT INTO fruits (name, healthy) VALUES (${newFruit.name}, ${newFruit.healthy}) RETURNING *`;
+
         return {
             statusCode: 201,
+            body: JSON.stringify(result)
         }
     }
 
